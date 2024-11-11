71d5c381778a2b76d4fa781f3f85cf51518ad855
EvyllenJoy <evyllenjoyce13@gmail.com>
2024-11-11 14:22:47 -0300

    11/11

--- aula4/js/ex04_1.js
+++ aula4/js/ex04_1.js
@@ -6,20 +6,19 @@ frm.addEventListener("submit", (e) => {
     const nome = frm.inNome.value
     const nota1 = Number(frm.inNota1.value)
     const nota2 = Number(frm.inNota2.value)
-
     const media = (nota1 + nota2) / 2
 
-    resp1.innerText = `Média: ${nome.toFixed(2)}`
-
-    if (media >= 7){
-        resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)!`
-        resp2.style.color = 'blue'
-    }else if (media >= 5){
-        resp2.innerText = `Foi por pouco ${nome}! Mas você ficou de recuperação!`
-        resp2.style.color = 'green'
-    }else{
-        resp2.innerText = `Deu ruim ${nome}! Você foi reprovado(a)!`
-        resp2.style.color = 'red'
+    resp1.innerText = `Média das Notas ${media.toFixed(2)}`
+    
+    if (media >=7){
+    resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
+    resp2.style.color = "blue"
+    } else if (media >= 5){
+    resp2.innerText = `Atenção ${nome}. Você está de recuperação!`
+    resp2.style.color = "green"
+    } else {
+    resp2.innerText = `Ops ${nome}... Você foi reprovado(a)`
+    resp2.style.color = "red"
     }
     e.preventDefault()
 })
