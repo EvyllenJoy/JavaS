766d9dbbb6ad004c10ad981de1d8ae7d8baa9217
EvyllenJoy <evyllenjoyce13@gmail.com>
2024-11-27 14:32:30 -0300

    27/11

---
+++ aula5/js/ex5_5.js
@@ -0,0 +1,23 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("outResp1")
+const resp2 = document.querySelector("outResp2")
+
+let resposta = ""
+let numConta = 0
+let valorTotal = 0
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const descricao = frm.inDescricao.value
+    const valor = Number(frm.inValor.value)
+
+    numConta++
+    valorTotal += valor
+    resposta += descricao + " - R$: " + valor.toFixed(2) +"\n"
+    resp1.innerText = `${resposta} -------------------------`
+    resp2.innerText = `${numConta} Contas(s) - Total R$: ${valorTotal.toFixed(2)}`
+    
+    //frm.inDescricao.value = ""
+    //frm.inValor.value = ""
+})
