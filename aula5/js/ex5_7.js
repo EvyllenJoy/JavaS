4299b9383d4ce45809014d7c95e0b4e1a9c257ac
EvyllenJoy <evyllenjoyce13@gmail.com>
2024-12-04 14:03:47 -0300

    04/12

---
+++ aula5/js/ex5_7.js
@@ -0,0 +1,18 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const num = Number(frm.inNumero.value)
+    let estrelas = "" //variável que vai concatenar as estrelas/traços
+
+    for (let i = 1; i <= num; i++){
+        if (i % 2 == 1){   
+            estrelas += "*"   //na posição impar, *
+        }else{
+            estrelas += "-"
+        }
+    }
+    resp.innerText = estrelas
+})
